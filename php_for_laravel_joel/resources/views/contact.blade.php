8678be6466096eeb00e4ef5ab0ac3da729aa6479
JoelIgle <joeliglesias@iesebre.com>
2023-12-05 19:41:54 +0100

    Prova

---
+++ php_for_laravel_joel/resources/views/contact.blade.php
@@ -0,0 +1,119 @@
+<x-layout>
+    <div class="min-h-full bg-white">
+        <div class="pb-32">
+
+        </div>
+
+        <main class="-mt-32">
+            <div class="bg-white py-24 sm:py-32">
+                <div class="mx-auto max-w-7xl px-6 lg:px-8">
+                    <div class="mx-auto max-w-2xl space-y-16 divide-y divide-gray-100 lg:mx-0 lg:max-w-none">
+                        <div class="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-3">
+                            <div>
+                                <h2 class="text-3xl font-bold tracking-tight text-gray-900">Contacta amb nosaltres</h2>
+                                <p class="mt-4 leading-7 text-gray-600">Aquí tens la nostra informació per a
+                                    contactar-nos.</p>
+                            </div>
+                            <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
+                                <div class="rounded-2xl bg-gray-50 p-10">
+                                    <h3 class="text-base font-semibold leading-7 text-gray-900">Col·labora</h3>
+                                    <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                        <div>
+                                            <dt class="sr-only">Email</dt>
+                                            <dd><a class="font-semibold text-indigo-600"
+                                                   href="mailto:collaborate@iesebre.com">collaborate@iesebre.com</a></dd>
+                                        </div>
+                                        <div class="mt-1">
+                                            <dt class="sr-only">Phone number</dt>
+                                            <dd>+1 (555) 905-2345</dd>
+                                        </div>
+                                    </dl>
+                                </div>
+                                <div class="rounded-2xl bg-gray-50 p-10">
+                                    <h3 class="text-base font-semibold leading-7 text-gray-900">Prensa</h3>
+                                    <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                        <div>
+                                            <dt class="sr-only">Email</dt>
+                                            <dd><a class="font-semibold text-indigo-600" href="mailto:press@iesebre.com">press@iesebre.com</a>
+                                            </dd>
+                                        </div>
+                                        <div class="mt-1">
+                                            <dt class="sr-only">Phone number</dt>
+                                            <dd>+1 (555) 905-3456</dd>
+                                        </div>
+                                    </dl>
+                                </div>
+                                <div class="rounded-2xl bg-gray-50 p-10">
+                                    <h3 class="text-base font-semibold leading-7 text-gray-900">Uneix'te a nosaltres</h3>
+                                    <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                        <div>
+                                            <dt class="sr-only">Email</dt>
+                                            <dd><a class="font-semibold text-indigo-600" href="mailto:careers@iesebre.com">careers@iesebre.com</a>
+                                            </dd>
+                                        </div>
+                                        <div class="mt-1">
+                                            <dt class="sr-only">Phone number</dt>
+                                            <dd>+1 (555) 905-4567</dd>
+                                        </div>
+                                    </dl>
+                                </div>
+                                <div class="rounded-2xl bg-gray-50 p-10">
+                                    <h3 class="text-base font-semibold leading-7 text-gray-900">Digues hola</h3>
+                                    <dl class="mt-3 space-y-1 text-sm leading-6 text-gray-600">
+                                        <div>
+                                            <dt class="sr-only">Email</dt>
+                                            <dd><a class="font-semibold text-indigo-600" href="mailto:hello@iesebre.com">hello@iesebre.com</a>
+                                            </dd>
+                                        </div>
+                                        <div class="mt-1">
+                                            <dt class="sr-only">Phone number</dt>
+                                            <dd>+1 (555) 905-5678</dd>
+                                        </div>
+                                    </dl>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="grid grid-cols-1 gap-x-8 gap-y-10 pt-16 lg:grid-cols-3">
+                            <div>
+                                <h2 class="text-3xl font-bold tracking-tight text-gray-900">Localitzacions</h2>
+                                <p class="mt-4 leading-7 text-gray-600">Ens pots trobar a aquestes localitzacions.</p>
+                            </div>
+                            <div class="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
+                                <div class="rounded-2xl bg-gray-50 p-10">
+                                    <h3 class="text-base font-semibold leading-7 text-gray-900">Los Angeles</h3>
+                                    <address class="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
+                                        <p>4556 Brendan Ferry</p>
+                                        <p>Los Angeles, CA 90210</p>
+                                    </address>
+                                </div>
+                                <div class="rounded-2xl bg-gray-50 p-10">
+                                    <h3 class="text-base font-semibold leading-7 text-gray-900">New York</h3>
+                                    <address class="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
+                                        <p>886 Walter Street</p>
+                                        <p>New York, NY 12345</p>
+                                    </address>
+                                </div>
+                                <div class="rounded-2xl bg-gray-50 p-10">
+                                    <h3 class="text-base font-semibold leading-7 text-gray-900">Toronto</h3>
+                                    <address class="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
+                                        <p>7363 Cynthia Pass</p>
+                                        <p>Toronto, ON N3Y 4H8</p>
+                                    </address>
+                                </div>
+                                <div class="rounded-2xl bg-gray-50 p-10">
+                                    <h3 class="text-base font-semibold leading-7 text-gray-900">Chicago</h3>
+                                    <address class="mt-3 space-y-1 text-sm not-italic leading-6 text-gray-600">
+                                        <p>726 Mavis Island</p>
+                                        <p>Chicago, IL 60601</p>
+                                    </address>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </main>
+    </div>
+
+</x-layout>
---
+++ php_for_laravel_joel/routes/web.php
@@ -0,0 +1,47 @@
+<?php
+
+use App\Models\Task;
+use Illuminate\Support\Facades\Route;
+
+
+
+Route::get('/', function () {
+//
+//   $task1 = Task::create([
+//        'name' => 'Task 1',
+//        'description' => 'Description 1',
+//        'completed' => 0
+//    ]);
+//   $task2 = Task::create([
+//        'name' => 'Task 2',
+//        'description' => 'Description 2',
+//        'completed' => 0
+//    ]);
+//
+//    $tasks = [
+//        $task1,
+//        $task2
+//    ];
+    return view('tasks',[
+        'tasks' => Task::all()
+
+    ]);
+});
+
+Route::get('/tasks', function () {
+    return view('tasks',[
+        'tasks' => []
+    ]);
+});
+
+Route::get('/users', function () {
+    return view('users');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
